fea3925f82bbf419a14f0286e6bdcc13f9a55b4f
nichro02 <nichro02@gmail.com>
2021-11-11 18:30:00 -0700

    Set up form to send data to backend

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,29 @@ function App() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  //handle user signup
+  async function registerUser(event) {
+    event.preventDefault()
+    
+    const response = await fetch('http://localhost:1337/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
     <div>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={registerUser}>
         <input
           value={name}
           onChange={e => setName(e.target.value)}
@@ -28,6 +47,9 @@ function App() {
           onChange={e => setPassword(e.target.value)}
           type='password'
           placeholder='Password'
+        />
+        <input
+          type='submit' value='Sign Up'
         />
       </form>
     </div>
