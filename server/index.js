6b797449c8ee24ec76a2a0b7619470cfbfd78569
nichro02 <nichro02@gmail.com>
2021-11-15 18:47:06 -0700

    Installed cors and mongoose packages, set up middleware to use cors and tell post route in server that data is in json format

--- server/index.js
+++ server/index.js
@@ -2,9 +2,22 @@
 const express = require('express')
 const app = express()
 
+//set up cors
+const cors = require('cors')
+app.use(cors())
+
+//json middleware
+app.use(express.json())
+
 //stub out test route
 app.get('/hello', (req, res) => {
     res.send('Hello World')
+})
+
+//POST route for registration info
+app.post('/api/register', (req, res) => {
+    console.log(req.body)
+    res.json({status: 'ok'})
 })
 
 app.listen(1337, () => {
