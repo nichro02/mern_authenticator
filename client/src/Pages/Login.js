cd090bf3d4ad1057dff61116a84227a9e12c6c6f
nichro02 <nichro02@gmail.com>
2021-11-23 19:16:20 -0700

    Created Login component

---
+++ client/src/Pages/Login.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import '../App.css';
+
+function Login() {
+    //useState to capture data entered into form
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+  
+    //handle user login
+    async function loginUser(event) {
+      event.preventDefault()
+      
+      const response = await fetch('http://localhost:1337/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      })
+      const data = await response.json()
+      console.log(data)
+    }
+  
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={loginUser}>
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            type='email'
+            placeholder='Email'
+          />
+          <input
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type='password'
+            placeholder='Password'
+          />
+          <input
+            type='submit' value='Log In'
+          />
+        </form>
+      </div>
+    );
+  }
+  
+  export default Login;
